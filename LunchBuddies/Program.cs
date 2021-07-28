36c1fa95923bddc30dbf1791cc653903128f47a9
thedigitalmenagerie <honeyraeswan@gmail.com>
2021-07-28 19:18:24 -0400

    first commit

---
+++ LunchBuddies/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LunchBuddies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
